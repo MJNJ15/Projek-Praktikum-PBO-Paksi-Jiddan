f6fc6013845ee485a804819b28a232974827d4e9
MJNJ15 <mjnjgaming@gmail.com>
2026-05-12 20:31:06 +0700

    Woke

---
+++ src/main/java/com/mycompany/sistem_informasi_bpjs/Sistem_Informasi_BPJS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistem_informasi_bpjs;
+
+/**
+ *
+ * @author MJNJ
+ */
+public class Sistem_Informasi_BPJS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
